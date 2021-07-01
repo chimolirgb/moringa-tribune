462156f206b672a88cabd7d68d33e746d2ff400f
Moringa School <chimolilucy@gmail.com>
2021-07-01 22:53:35 +0300

    heroku deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -95,9 +95,9 @@ if config('MODE')=="dev":
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'USER': 'postgres',
-        # 'PASSWORD':'Access',
+         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         # 'USER': 'postgres',
+         # 'PASSWORD':'Access',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
