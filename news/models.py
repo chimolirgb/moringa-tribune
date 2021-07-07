da2efdc6bcf60b288cfa3e98bd6c8e5ee38a77a2
Moringa School <chimolilucy@gmail.com>
2021-07-07 19:26:46 +0300

    all files

--- news/models.py
+++ news/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 
 # Create your models here.
 class Editor(models.Model):
@@ -22,11 +25,11 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete=models.CASCADE,) 
+    post =  HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE,) 
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/',null = True)
+    article_image = models.ImageField(upload_to = 'articles/',blank = True)
 
     @classmethod
     def search_by_title(cls,search_term):
@@ -38,4 +41,8 @@ class Article(models.Model):
         today = dt.date.today()
         news = cls.objects.filter(pub_date__date = today)
         return news
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()      
       
--- tribune/settings.py
+++ tribune/settings.py
@@ -11,14 +11,21 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
+from pathlib import Path
 from decouple import config,Csv
+
+
+
+#ALLOWED_HOSTS=['.localhost', '.herokuapp.com', '.127.0.0.1']
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -36,7 +43,6 @@ SECRET_KEY = 'yx%)=o6lnd6mnli16&+b!z67$c=8cu(&-%n3#zj)r&l-2&fga4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -50,6 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+  
 ]
 
 MIDDLEWARE = [
@@ -94,7 +102,7 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 if config('MODE')=="dev":
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
          # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
          # 'USER': 'postgres',
          # 'PASSWORD':'Access',
@@ -103,6 +111,7 @@ if config('MODE')=="dev":
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': '',
+
     }
 }
 
@@ -117,7 +126,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -150,23 +159,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# configuring the location for media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Configure Django App for Heroku.
-django_heroku.settings(locals())
